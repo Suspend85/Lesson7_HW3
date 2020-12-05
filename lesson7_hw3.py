d9d56d8facdf4bd9dcc5b7f18a345f6deafee1f7
Dmi_Krop <melomanko@gmail.com>
2020-12-05 12:31:37 +0700

    changed the process of forming the file_list

--- lesson7_hw3.py
+++ lesson7_hw3.py
@@ -1,15 +1,19 @@
-def file_parse():
-    result = []
-    num_of_files = 3  # Set number of file we need to parse
+import glob
 
-    for i in range(1, num_of_files+1):  # generate name of file to read him
-        filename = '{0}.txt'.format(i)
-        with open(filename, encoding='utf-8') as f:
+
+def file_parse():
+    file_list = []
+    for file in glob.glob("*.txt"):
+        if file != 'res.txt':  # Excluding this file from the file_list
+            file_list.append(file)
+    result = []
+    for file_name in file_list:
+        with open(file_name, encoding='utf-8') as f:
             lines = f.readlines()
             lines_count = len(lines)
-            result.append([filename, lines_count, ''.join(lines)])
-    # Sorting by second element (num of lines) of list
-    result.sort(key=lambda x: x[1])
+            result.append([file_name, lines_count, ''.join(lines)])
+            # Sorting by second element (num of lines) of list
+            result.sort(key=lambda x: x[1])
     # Writing lines to file
     with open('res.txt', 'w', encoding='utf-8') as f:
         for i in result:
